ec645b041aef20b781cea8a3bccd505c54e215c0
Mohamed El Sayed Ahmed Ismail <darkangel7863@gmail.com>
2025-05-04 23:17:25 +0300

    creating the redux store and slicing the features

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
